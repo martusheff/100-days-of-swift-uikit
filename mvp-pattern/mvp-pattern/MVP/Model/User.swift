9f60b394d99869f3c9dc87b3cad7841482841dd5
Andronick Martusheff <andronick@outlook.com>
2022-03-29 13:44:06 -0700

    MVP Practice: get sample app written.

---
+++ mvp-pattern/mvp-pattern/MVP/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  mvp-pattern
+//
+//  Created by andronick martusheff on 3/29/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let username: String
+}
