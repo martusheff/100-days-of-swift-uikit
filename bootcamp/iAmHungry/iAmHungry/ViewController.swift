bb1eb4376ae7bd0ba55141efdad8f75454d10f11
Andronick Martusheff <andronick@outlook.com>
2022-04-06 16:57:22 -0700

    Section 3: iAmHungry app (putting whatI learned from section 2 into practice on my own.)

---
+++ bootcamp/iAmHungry/iAmHungry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iAmHungry
+//
+//  Created by andronick martusheff on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
