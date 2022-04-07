14c1814adfbe440fc688fa6eb6ff5a168687b021
Andronick Martusheff <andronick@outlook.com>
2022-04-07 00:55:54 -0700

    Section 4: Dicee rolling app. IBOutlets & IBActions

---
+++ bootcamp/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // IBOutlet lets me reference a UI element
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    @IBOutlet weak var diceTotalLabelView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // WHO           WHAT    VALUE
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        
+    }
+
+
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        // Dice Asset Name Representation
+        let dice = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        
+        // Get 2 Random Numbers
+        let randomRollOne = Int.random(in: 0...5)
+        let randomRollTwo = Int.random(in: 0...5)
+        let rollSum = randomRollOne + randomRollTwo + 2
+        
+        // Update Dice on UI
+        diceImageViewOne.image = UIImage(named: dice[randomRollOne])
+        diceImageViewTwo.image = UIImage(named: dice[randomRollTwo])
+        
+        // Update Rolled Amount on UI
+        diceTotalLabelView.text = "You rolled a \(rollSum)"
+        //print(rollSum)
+    }
+    
+}
+
