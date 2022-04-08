a9740ae22b848cae86e511218c812ebb81574f2c
Andronick Martusheff <andronick@outlook.com>
2022-04-07 21:14:44 -0700

    Stitch: added easter egg.

--- bootcamp/stitch/stitch/ViewController.swift
+++ bootcamp/stitch/stitch/ViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var cookieImageView: UIImageView!
+    @IBOutlet weak var secretBunnyImageView: UIImageView!
+    @IBOutlet weak var secretMessageLabel: UILabel!
+    
     var bites = 1
+    var secret = "xoxooxooo"
+    var sequence = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +27,31 @@ class ViewController: UIViewController {
         let cookieToDisplay = bites + 1 > 3 ? 1 : bites + 1
         bites = cookieToDisplay
         cookieImageView.image = UIImage(named: "cookie\(cookieToDisplay)")
+        sequence = ""
+        secretUnlocked()
+        
+        
+    }
+    @IBAction func oSecretButtonTap(_ sender: UIButton) {
+        sequence += "o"
+        secretUnlocked()
+        print(sequence)
     }
     
+    @IBAction func xSecretButtonTap(_ sender: UIButton) {
+        sequence += "x"
+        secretUnlocked()
+        print(sequence)
+    }
+    
+    func secretUnlocked() {
+        if secret == sequence {
+            secretBunnyImageView.alpha = 1.0
+            secretMessageLabel.alpha = 1.0
+        } else {
+            secretBunnyImageView.alpha = 0.0
+            secretMessageLabel.alpha = 0.0
+        }
+    }
 }
 
