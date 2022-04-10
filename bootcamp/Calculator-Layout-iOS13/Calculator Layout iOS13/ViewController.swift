f4964bc935e106a5d54bb54a834c4cf480b4cd16
Andronick Martusheff <andronick@outlook.com>
2022-04-10 10:48:15 -0700

    Implement calculator app functionality.

--- bootcamp/Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
+++ bootcamp/Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 1
+    var entry: String = ""
+    var entries: [Any] = []
+    var operation = Operation.none
+    
+    enum Operation {
+        case addition
+        case none
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numKeyPressed(_ sender: UIButton) {
+        entry += "\(sender.tag)"
+        resultLabel.text = entry
+    }
+    
+    
+    @IBAction func operationKeyPressed(_ sender: UIButton) {
+        entries.append(entry)
+        entry = ""
+        switch sender.currentTitle {
+        case "+":
+            entries.append(Operation.addition)
+            operation = Operation.addition
+        default:
+            print("ERROR: Incorrect Key Press")
+        }
+    }
+    
+    @IBAction func clearKeyPressed(_ sender: UIButton) {
+        result = 0
+        entry = ""
+        entries = []
+        resultLabel.text = "0"
+    }
+    
+    @IBAction func equalKeyPressed(_ sender: UIButton) {
+        entries.append(entry)
+        entry = ""
+        switch operation {
+        case .addition:
+            let num1: Int = Int(entries[0] as! String) ?? 0
+            let num2: Int = Int(entries[2] as! String) ?? 0
+            result = num1 + num2
+            resultLabel.text = "\(result)"
+            
+        case .none:
+            print("ERROR: No operation selected.")
+        }
+    }
+    
 }
 
