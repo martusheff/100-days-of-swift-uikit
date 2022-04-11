25e534e152b4d8ab80558c11225aaf909e4e7d75
Andronick Martusheff <amartush@amartush21631.starbucks.net>
2022-04-11 10:39:29 -0700

    extracting data from txt file

---
+++ random/five-letter-words.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var words: [String] = [String]()
+
+do {
+    guard let file = Bundle.main.url(forResource: "words", withExtension: "txt") else { fatalError() }
+    let text = try String(contentsOf: file, encoding: String.Encoding.utf8)
+words = text.components(separatedBy: "\n")
+    
+} catch {
+    print("Error: Unable to open file.")
+}
+
+let count = words.count
+let random: String = words[Int.random(in: 0 ..< count)]
+print(random)
